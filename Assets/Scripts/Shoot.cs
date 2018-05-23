832db36421e1737aa747a4a4d5b884a5c2969b06
Arvin（张心彧） <1024899756@qq.com>
2018-05-23 16:37:37 +0800

    2018.5.23
    1

---
+++ Assets/Scripts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+
+    public GameObject bullet;
+    public float speed = 40;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if ( Input.GetMouseButtonDown(0) )
+        {
+            GameObject b =  GameObject.Instantiate( bullet, transform.position, transform.rotation );
+            Rigidbody rgb = b.GetComponent<Rigidbody>();
+            rgb.velocity = transform.forward * speed;
+        }
+
+	}
+}
